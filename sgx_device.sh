09cc39735b3fe82f9d610d29116cea9703bb2d27
vasyl_lukashuk <vas77794@gmail.com>
2021-04-22 17:26:12 +0200

    Update to 5.3.2

--- sgx_device.sh
+++ sgx_device.sh
@@ -2,7 +2,7 @@
 
 function determine_sgx_device {
     export SGXDEVICE="/dev/sgx"
-    export MOUNT_SGXDEVICE="-v /dev/sgx/:/dev/sgx"
+    export MOUNT_SGXDEVICE="--device=/dev/sgx"
     if [[ ! -e "$SGXDEVICE" ]] ; then
         export SGXDEVICE="/dev/isgx"
         export MOUNT_SGXDEVICE="--device=/dev/isgx"
