af0324c066dd68671632dc5057c6bbb5bb09f40b
vasyl_lukashuk <vas77794@gmail.com>
2021-05-07 21:15:29 +0200

    Update to 5.3.3

--- sconify.sh
+++ sconify.sh
@@ -2,7 +2,7 @@
 
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.3.2 sconify_iexec \
+            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.3.3 sconify_iexec \
             --name=helloworldPython \
             --from=registry.scontain.com:5050/sconecuratedimages/iexecsgx:hello-world-python \
             --to=hello-world-python-scone \
@@ -12,5 +12,4 @@ docker run -it --rm \
             --heap="128m" \
             --dlopen="2" \
             --no-color \
-            --verbose \
             --command="python3 /app/helloworld.py"
