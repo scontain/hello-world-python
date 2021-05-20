202f8d2ffeaad4e17d2591706cf376ece1738189
vasyl_lukashuk <vas77794@gmail.com>
2021-05-20 17:40:12 +0200

    Update to 5.4.0

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ echo "Run sconified image..." && \
 docker run -it --rm --network=host $MOUNT_SGXDEVICE \
 -eSCONE_VERSION=1 \
 -eSCONE_MODE=hw \
--eSCONE_LOG="7" \
+-eSCONE_LOG="DEBUG" \
 -ePATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
 hello-world-python-scone python3 /app/helloworld.py
--- sconify.sh
+++ sconify.sh
@@ -2,7 +2,7 @@
 
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.3.3 sconify_iexec \
+            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.4.0 sconify_iexec \
             --name=helloworldPython \
             --from=registry.scontain.com:5050/sconecuratedimages/iexecsgx:hello-world-python \
             --to=hello-world-python-scone \
